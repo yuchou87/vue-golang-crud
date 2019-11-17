62369bee6b724ae206b1474ac58efcc5d96fc102
yuchou <yuchou87@gmail.com>
2019-11-17 09:35:40 +0800

    [A] Ping組件

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,22 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+// eslint-disable-next-line import/extensions
+import Ping from '../components/Ping';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/ping',
+    name: 'ping',
+    component: Ping,
   },
 ];
 
