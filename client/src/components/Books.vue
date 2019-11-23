7a67b6b7788f75293f6fd422e67dab0f749d7279
yuchou <yuchou87@gmail.com>
2019-11-23 14:56:38 +0800

    [U] update操作

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -26,7 +26,14 @@
           </td>
           <td>
             <div class="btn-group" role="group">
-              <button type="button" class="btn btn-warning btn-sm">Update</button>
+              <button
+                type="button"
+                class="btn btn-warning btn-sm"
+                v-b-modal.book-update-modal
+                @click="editBook(book)"
+                >
+                Update
+              </button>
               <button type="button" class="btn btn-danger btn-sm">Delete</button>
             </div>
           </td>
@@ -62,6 +69,44 @@
     <b-button type="reset" variant="danger">Reset</b-button>
   </b-form>
   </b-modal>
+
+  <b-modal ref="editBookModal" id="book-update-modal" title="Update" hide-footer>
+    <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+      <b-form-group id="form-title-edit-group"
+                    label="Title:"
+                    label-for="form-title-edit-input"
+                    >
+        <b-form-input id="form-title-edit-input"
+                      type="text"
+                      v-model="editForm.title"
+                      required
+                      placeholder="Enter title"
+                      >
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="form-author-edit-group"
+                    label="Author:"
+                    label-for="form-author-edit-input"
+                    >
+        <b-form-input id="form-author-edit-input"
+                      type="text"
+                      v-model="editForm.author"
+                      required
+                      placeholder="Enter author"
+                      >
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="form-read-edit-group">
+        <b-form-checkbox-group v-model="editForm.status" id="form-checks">
+          <b-form-checkbox value="true">Read?</b-form-checkbox>
+        </b-form-checkbox-group>
+      </b-form-group>
+      <b-button-group>
+        <b-button type="submit" variant="primary">Update</b-button>
+        <b-button type="reset" variant="danger">Cancel</b-button>
+      </b-button-group>
+    </b-form>
+  </b-modal>
 </div>
 </template>
 
@@ -75,6 +120,12 @@ export default {
     return {
       books: [],
       addBookForm: {
+        title: '',
+        author: '',
+        status: [],
+      },
+      editForm: {
+        id: '',
         title: '',
         author: '',
         status: [],
@@ -112,10 +163,28 @@ export default {
           this.getBooks();
         });
     },
+    updateBook(payload, bookID) {
+      const path = `http://localhost:5000/book/${bookID}`;
+      axios.put(path, payload)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Book updated!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.error(error);
+          this.getBooks();
+        });
+    },
     initForm() {
       this.addBookForm.title = '';
       this.addBookForm.author = '';
       this.addBookForm.status = [];
+      this.editForm.id = '';
+      this.editForm.title = '';
+      this.editForm.author = '';
+      this.editForm.status = [];
     },
     onSubmit(evt) {
       evt.preventDefault();
@@ -134,6 +203,27 @@ export default {
       evt.preventDefault();
       this.$refs.addBookModal.hide();
       this.initForm();
+    },
+    editBook(book) {
+      this.editForm = book;
+    },
+    onSubmitUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editBookModal.hide();
+      let status = false;
+      if (this.editForm.status[0]) status = true;
+      const payload = {
+        title: this.editForm.title,
+        author: this.editForm.author,
+        status,
+      };
+      this.updateBook(payload, this.editForm.id);
+    },
+    onResetUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editBookModal.hide();
+      this.initForm();
+      this.getBooks();
     },
   },
   created() {
